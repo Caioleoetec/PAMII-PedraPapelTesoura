6b5274499ef2ce19166bda30cf63ff36bc5690ba
caioleo <caioleo.etec@gmail.com>
2024-05-21 12:41:32 -0300

    Criando Pedra Papel Tesoura

---
+++ PedraPapelTesoura/PedraPapelTesoura/Models/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedraPapelTesoura.Models
+{
+    internal class Player
+    {
+        private string Name {  get; set; }
+        private EnumChoice choice;
+        private int score { get; set; }
+        
+        void play() {
+            // 1 - Paper; 2 - Scissors; 3 - Rock 
+            int escolha = Random.Shared.Next(3);
+            if(escolha == 1){
+                choice = EnumChoice.PAPER;
+            }
+            if(escolha == 2)
+            {
+                choice = EnumChoice.SCISSORS;
+            }
+            if(escolha == 3) {
+                choice = EnumChoice.ROCK;
+            }
+        }
+        void play(EnumChoice choice) { this.choice = choice;}
+    }
+}
---
+++ PedraPapelTesoura/PedraPapelTesoura/Views/PlayerView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace PedraPapelTesoura
+{
+    public partial class PlayerView : ContentPage
+    {
+        int count = 0;
+
+        public PlayerView()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
